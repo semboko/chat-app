98e89e54de36851514f1dac3013dd04aeb1aee54
Simon Bondarenko <bondarenko@vt.edu>
2024-01-26 20:31:03 -0600

    Changed the layout

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,15 +1,25 @@
 import './auth.css';
 
+import { FaRegUser, FaUnlock } from 'react-icons/fa6';
+
 export function SignUpPage(){
     return (
-        <div className="form-container">
-            <header>Sign Up</header>
-            <form>
-                <input type="text" placeholder='Username' />
-                <input type="password" placeholder='Password' />
-                <p>Already have an account? <a href="#">Sign In</a></p>
-                <button>Submit</button>
-            </form>
+        <div className="auth-page">
+            <div className="form-container">
+                <header>Sign Up</header>
+                <form>
+                    <div className="text-input-container">
+                        <input className="auth-text-input" type="text" placeholder='Username' required />
+                        <label className="text-input-icon"><FaRegUser /></label>
+                    </div>
+                    <div className="text-input-container">
+                        <input className="auth-text-input" type="password" placeholder='Password' required />
+                        <label className="text-input-icon"><FaUnlock /></label>
+                    </div>
+                    <p>Already have an account? <a href="#">Sign In</a></p>
+                    <button className="submit-button">Create Account</button>
+                </form>
+            </div>
         </div>
     )
 }
